9c48b5a3dc309e4ea27c5b3ccadd7a949b09fca6
IngErnestoMV <ingenieroensistemasernestomv@gmail.com>
2022-07-31 02:10:41 -0500

    Crear HTML con createElement

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,29 @@
-// querySelector
+/*  querySelector   */
 const heading = document.querySelector('.header__texto h2')// Retorna 0 o 1 elemento
 heading.textContent = 'Nuevo Heading'; // Coambia el texto
 heading.classList.add('nueva-clase'); // Agrega una clase
-console.log(heading);
+// console.log(heading);
 
-// querySelectorAll
+/*  querySelectorAll    */
 const enlaces = document.querySelectorAll('.navegacion a'); // Retorna de 0 a todos los elementos
-console.log(enlaces[0]);
+// console.log(enlaces[0]);
 
 enlaces[0].textContent = 'Nuevo Texto para Enlace';
 enlaces[0].classList.add('nueva-clase');
-enlaces[0].classList.remove('navegacion__enlace');
+//  enlaces[0].classList.remove('navegacion__enlace');
 
-// getElementById
-
+/*  getElementById  */
 const heading2 = document.getElementById('heading');
-console.log(heading2);
+// console.log(heading2);
+
+/*  Generar un nevo enlace   */
+const nuevoEnlace = document.createElement('A'); // Se recomienda poner las etiquetas en mayusculas en JavaScript
+//  Agregar el href
+nuevoEnlace.href = 'nuevo-enlace.html';
+//  Agregar el texto
+nuevoEnlace.textContent = 'Un Nuevo Enlace';
+//  Agregar la clase
+nuevoEnlace.classList.add('navegacion__enlace');
+navegacion.appendChild(nuevoEnlace);
+
+console.log(nuevoEnlace);
